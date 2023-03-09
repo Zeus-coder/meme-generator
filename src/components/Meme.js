df8c55b7d47261bb60ff972cee857befda7bd72f
OLuwapelumi <freshtunz58@gmail.com>
2023-03-09 01:05:54 +0100

    meme generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Meme (){
     return(
-        <section>
+        <section className="meme-generator-section">
             <input type="text" className="first-text" placeholder="top text"></input>
             <input type="text" className="second-text" placeholder="bottom text"></input>
-            <button id="btn">Get a new meme image  🖼</button>
+            <button id="btn">Get a new meme image 🖼</button>
         </section>
     )
 }
